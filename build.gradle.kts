efb36fe2973e915b67ca76786e14b3d34121b3e7
ArcticAquila <franklinazriel6942@outlook.com>
2024-04-12 15:36:59 +0700

    Add PaperMC API Library 1.20.4-R0-1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,12 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
 }
 
 tasks.test {
